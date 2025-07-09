72ee884010f703358ef7180d57eb4283105c7709
rkdaas <gupta.ritik2002@gmail.com>
2025-07-09 14:05:50 +0000

    Script-2 Read input and print to console back

---
+++ Shell-Scripting/Script-2.sh
@@ -0,0 +1,19 @@
+# Create a script that takes user input (using read) and prints it back to the console
+
+###########################Start of the Script ###########################
+
+#!/bin/bash
+echo "Please enter something"
+read user_input
+
+# Print that input back to the console 
+
+echo "You entered: $user_input"
+
+###########################End of the Script #############################
+
+# save it to a file named Script-2.sh
+# and then execute the following command in your terminal:
+# chmod +x Script-2.sh
+# ./Script-2.sh
+# This will make the script executable.
