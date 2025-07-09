f59bd4373df5b0bf7e416b431c2b5c458a3935ef
rkdaas <gupta.ritik2002@gmail.com>
2025-07-09 13:53:22 +0000

    Print hello-world to the console

---
+++ Shell-Scripting/Script-1.sh
@@ -0,0 +1,19 @@
+# Write a simple shell script that prints "Hello, World!" to the console
+
+#######################Start of Script#######################
+#!/bin/bash
+
+echo "Hello,world!" 
+
+##############################End of Script##################
+
+
+
+############ To run this script, save it to a file named Script-1.sh############
+
+# and then execute the following command in your terminal:
+# chmod +x Script-1.sh
+# ./Script-1.sh
+
+# This will make the script executable and then run it, printing "Hello, World!" to the console.
+# Make sure you have the necessary permissions to execute the script.
