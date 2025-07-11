51d5959a42b6ad6a7f5882d8a2479697b997367e
rkdaas <gupta.ritik2002@gmail.com>
2025-07-11 11:45:06 +0000

    Script-9 Print from 1 to 10 with for loop

---
+++ Shell-Scripting/Script-9.sh
@@ -0,0 +1,15 @@
+# Create a script that demonstrates the usage of a for loop to print numbers from 1 to 10.
+
+#######################################Start of the script#######################################
+#!/bin/bash
+# Script to print numbers from 1 to 10 using a for loop
+for i in {1..10}
+do
+    echo "Number: $i"
+done
+#######################################End of the script#########################################
+# End of the script
+# Save this script as Script-9.sh and run it to see the output.
+# Usage: chmod +x Script-9.sh && ./Script-9.sh
+# This script will print each number from 1 to 10 on a new line.
+# Ensure you have execute permissions on the script file before running it.
