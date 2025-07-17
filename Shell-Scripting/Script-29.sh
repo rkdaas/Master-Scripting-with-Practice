c2547dfac5cff6fe9c153eea6220acff78d35dfa
rkdaas <gupta.ritik2002@gmail.com>
2025-07-17 14:32:32 +0000

    Script-29 script to read with a default value if no input is given in 5 seconds

---
+++ Shell-Scripting/Script-29.sh
@@ -0,0 +1,14 @@
+# Write a script to read with a default value if no input is given in 5 seconds 
+
+########################################Shell Script 29########################################
+#!/bin/bash
+echo " Enter a value (default is "Hello world") " 
+read -t 5 input 
+
+
+if [ -z "$input" ]
+then 
+    input="Hello world"
+fi
+echo " You entered nothing so the default value of input is $input " 
+########################################Shell Script 29########################################)
