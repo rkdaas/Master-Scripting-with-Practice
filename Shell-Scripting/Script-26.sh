b51baacb6d72ef9eba4e1ad025c4889a085b9ca1
rkdaas <gupta.ritik2002@gmail.com>
2025-07-17 14:13:02 +0000

    Script-26 Loop through the arguments and print them - udpated

--- Shell-Scripting/Script-26.sh
+++ Shell-Scripting/Script-26.sh
@@ -5,11 +5,18 @@
 First_argument=$1
 Second_argument=$2
 Third_argument=$3
+Script_name=$(basename "$0")
+
 my_array=("$First_argument" "$Second_argument" "$Third_argument")
 
 
 # Loop through the arguments and print each one
+
+
+
 for arg in "${my_array[@]}"; do 
     echo "Argument: $arg"
 done
+
+echo "Script name: $Script_name"
 
